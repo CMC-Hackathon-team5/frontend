57376f291a3ed230617151a5fc8ddef63bd2b255
gabi <70315572+Limgabi@users.noreply.github.com>
2023-01-07 17:24:54 +0900

    feat: 캘린더

--- App.js
+++ App.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import BottomNavigation from './src/BottomNavigation';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import CalendarView from './src/components/CalendarView';
+import SignIn from './src/routes/SignIn';
 import SignUp from './src/routes/SignUp';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -13,9 +24,16 @@ const App = () => {
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name='root' component={BottomNavigation} />
         <Stack.Screen name='signUp' component={SignUp} />
+        <Stack.Screen name='CalendarView' component={CalendarView} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  }
+});
 
 export default App;
--- src/BottomNavigation.jsx
+++ src/BottomNavigation.jsx
@@ -3,6 +3,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Text } from 'react-native';
 import MyPage from './routes/MyPage';
 import SignUp from './routes/SignUp';
+import CalendarView from './components/CalendarView';
 
 const Tab = createBottomTabNavigator();
 
@@ -19,7 +20,7 @@ const BottomNavigation = () => {
       // screenOptions={{ headerShown: false }}
       >
         <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Search" component={Search} />
+        <Tab.Screen name="Search" component={CalendarView} />
         <Tab.Screen name="SignUp" component={SignUp} />
         <Tab.Screen name="Message" component={MyPage} />
       </Tab.Navigator>
