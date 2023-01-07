9a2701101eec8fccf7a230e1d4f292f25e31ecf2
bgyoonseo <73841260+bgyoons@users.noreply.github.com>
2023-01-07 16:57:43 +0900

    feat: 네비게이션 추가

---
+++ src/BottomNavigation.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Text } from 'react-native';
+import MyPage from './routes/MyPage';
+import SignUp from './routes/SignUp';
+
+const Tab = createBottomTabNavigator();
+
+function Home() {
+  return <Text>Home</Text>;
+}
+function Search() {
+  return <Text>Search</Text>;
+}
+
+const BottomNavigation = () => {
+  return (
+      <Tab.Navigator initialRouteName="Home"
+      // screenOptions={{ headerShown: false }}
+      >
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Search" component={Search} />
+        <Tab.Screen name="SignUp" component={SignUp} />
+        <Tab.Screen name="Message" component={MyPage} />
+      </Tab.Navigator>
+  )
+}
+
+export default BottomNavigation;
